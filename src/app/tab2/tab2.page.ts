ec7e7f589d5c7557da783c807590abcede9c9995
Victor Machado <joelvictor2012@gmail.com>
2019-01-15 01:18:38 -0300

    Update tabs

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  public tasks = localStorage.tasks ? JSON.parse(localStorage.tasks) : [];
+  public taskForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.taskForm = this.fb.group({
+      isFormVisible: [false],
+      name: [''],
+      description: ['', Validators.required],
+    });
+  }
+
+  public saveTask() {
+    this.tasks.push({
+      name: this.taskForm.get('name').value,
+      description: this.taskForm.get('description').value
+    });
+
+    this.taskForm.reset();
+    
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+
+}
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+  public counter = localStorage.counter ? JSON.parse(localStorage.counter) : 0;
+
+  public addOneToCounter() {
+    this.counter += 1;
+    
+    localStorage.setItem('counter', JSON.stringify(this.counter));
+  }
+
+}
